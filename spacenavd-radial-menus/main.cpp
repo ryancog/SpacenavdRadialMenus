af247a96ac98e537b73c7c2105ef2a53dfb3222e
ryryog25 <ryancog@icloud.com>
2023-09-04 17:25:12 -0500

    Added FusionCommands.txt

--- spacenavd-radial-menus/main.cpp
+++ spacenavd-radial-menus/main.cpp
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     if (!checkArg("--no-daemon", argc, argv)) {
-        daemon(0, 1);
+        daemon(1, 0);
         freopen("/var/log/spacenavd-radial-menus.log", "w", stdout);
         LOG("Running as daemon...");
     }
@@ -75,7 +75,7 @@ int main(int argc, char *argv[])
         .right = {
             .name         = "Circle",
             .keystroke    = "C",
-            .fusionAction = "DrawCircle"
+            .fusionAction = "CircleCenterRadius"
         }
     };
 
@@ -99,7 +99,7 @@ int main(int argc, char *argv[])
         spnav_event sev;
 
         while(!false) {
-            std::this_thread::sleep_for(std::chrono::milliseconds(50));
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
             if (spnav_poll_event(&sev) == SPNAV_EVENT_BUTTON && sev.button.press == 0) {
                 QPoint cLoc = QCursor::pos();
                 menus[sev.button.bnum]->setGeometry(
