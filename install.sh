e17b690b4ce8dd733eb3c58a6873820cf7b16bb9
ryryog25 <ryancog@icloud.com>
2023-09-04 22:58:26 -0500

    Updated config.h w/ info

--- install.sh
+++ install.sh
@@ -3,22 +3,20 @@
 cd
 echo "Installing dependencies..."
 sudo apt-get -y install git libxtst-dev > /dev/null # There's a lot that should be in this list...
-git clone https://github.com/ryryog25/SpacenavdRadialMenus.git > /dev/null
+git clone --quiet https://github.com/ryryog25/SpacenavdRadialMenus.git > /dev/null
 
 echo "Opening Fusion Command List..."
 xdg-open https://raw.githubusercontent.com/ryryog25/SpacenavdRadialMenus/main/FusionCommands.txt
 
 echo "Opening config.h for editing..."
-cd SpacenavdRadialMenus
-nano spacenavd-radial-menus/config.h
+cd SpacenavdRadialMenus/spacenavd-radial-menus
+nano config.h
 
-echo "Moving to build..."
+echo "Building spacenavd-radial-menus..."
 mkdir build
 cd build
-
-echo "Building in ${pwd}."
-cmake ..
-make
+cmake .. > /dev/null
+make > /dev/null
 
 echo "Copying executable..."
 sudo cp ~/SpacenavdRadialMenus/spacenavd-radial-menus/build/spacenavd-radial-menus /usr/local/bin/
@@ -26,4 +24,4 @@ sudo cp ~/SpacenavdRadialMenus/spacenavd-radial-menus/build/spacenavd-radial-men
 echo "Copying resources..."
 sudo mkdir /usr/local/share/spacenavd-radial-menus && sudo cp -r ~/SpacenavdRadialMenus/spacenavd-radial-menus/resources /usr/local/share/spacenavd-radial-menus/
 
-echo -e "Spacenavd Radial Menus can now be started via \e[31mspacenavd-radial-menus [args]"
+echo -e "Spacenavd Radial Menus can now be started via \e[32mspacenavd-radial-menus [args]"
--- spacenavd-radial-menus/config.h
+++ spacenavd-radial-menus/config.h
@@ -1,6 +1,30 @@
 #pragma once
 #include "widget.h"
 
+/*
+*    Below is the spacenavd-radial-menus button action config.
+*
+*    Each entry in the actions[] array corresponds to a radial menu for a SpaceMouse button.
+*    Each entry contains several fields for the .top, .bottom, .left, and .right menus
+*        (Shown when corresponding button is triggered)
+*
+*        .name is simply the name to be shown on the radial menu
+*        .keystroke is the keysym to be executed when running normally
+*        .fusionAction is the command in Fusion360 to be run when running with --fusion360
+*            - The list of Fusion360 commands should have opened in your browser (or can be accessed on the repo)
+*            - Many of these commands are not intended to be executed at any time, and care should be used
+*              when executing them, the file structure is [Name], [Command ID], where the Command ID is
+*              what is used for the fusionAction.
+*
+*    Additional buttons can be enabled by adding a new struct to the array, and filling out the corresponding
+*    fields for the new struct.
+*
+*
+*    For more information, please reference the README located on the github repo.
+*
+*/
+
+
 Widget::ButtonActions actions[] = {
     { // Button 0 (Left)
         .top = {
