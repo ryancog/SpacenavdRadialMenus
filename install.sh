979dc4987f7a5f06d21bf5aef784bd74f3d0e46b
ryryog25 <ryancog@icloud.com>
2023-09-04 22:40:49 -0500

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,29 +1,29 @@
 #!/bin/bash 
 
 cd
-echo "Installing dependencies...\n"
+echo "Installing dependencies..."
 sudo apt-get -y install git libxtst-dev > /dev/null # There's a lot that should be in this list...
 git clone https://github.com/ryryog25/SpacenavdRadialMenus.git > /dev/null
 
-echo "Opening Fusion Command List...\n"
+echo "Opening Fusion Command List..."
 xdg-open https://raw.githubusercontent.com/ryryog25/SpacenavdRadialMenus/main/FusionCommands.txt
 
-echo "Opening config.h for editing...\n"
+echo "Opening config.h for editing..."
 cd SpacenavdRadialMenus
 nano spacenavd-radial-menus/config.h
 
-echo "Moving to build...\n"
+echo "Moving to build..."
 mkdir build
 cd build
 
-echo "Building in ${pwd}.\n"
+echo "Building in ${pwd}."
 cmake ..
 make
 
-echo "Copying executable...\n"
+echo "Copying executable..."
 sudo cp ~/SpacenavdRadialMenus/spacenavd-radial-menus/build/spacenavd-radial-menus /usr/local/bin/
 
-echo "Copying resources...\n"
+echo "Copying resources..."
 sudo mkdir /usr/local/share/spacenavd-radial-menus && sudo cp -r ~/SpacenavdRadialMenus/spacenavd-radial-menus/resources /usr/local/share/spacenavd-radial-menus/
 
 echo -e "Spacenavd Radial Menus can now be started via \e[31mspacenavd-radial-menus [args]"
