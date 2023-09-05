04c271f6aee949733c9c685dd15b3e55fbed605a
ryryog25 <ryancog@icloud.com>
2023-09-04 22:23:53 -0500

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+
+cd
+sudo apt-get -y install git libxtst-dev # There's a lot that should be in this list...
+git clone https://github.com/ryryog25/SpacenavdRadialMenus.git
+
+xdg-open https://raw.githubusercontent.com/ryryog25/SpacenavdRadialMenus/main/FusionCommands.txt
+cd SpacenavdRadialMenus
+nano spacenavd-radial-menus/config.h
+
+mkdir build
+cd build
+
+cmake ..
+make
+
+sudo cp ~/SpacenavdRadialMenus/spacenavd-radial-menus/build/spacenavd-radial-menus /usr/local/bin/
+
+sudo mkdir /usr/local/share/spacenavd-radial-menus && sudo cp -r ~/SpacenavdRadialMenus/spacenavd-radial-menus/resources /usr/local/share/spacenavd-radial-menus/
+
+echo << "Spacenavd Radial Menus can now be started via `spacenavd-radial-menus [args]`"
